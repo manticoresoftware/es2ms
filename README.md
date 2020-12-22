bfba010d2d3393341e7689cd05b9c32cad2e282d
Adrian Nuta <adriannuta@gmail.com>
2020-12-22 18:06:24 +0100

    add more cli options and docs

--- README.md
+++ README.md
@@ -31,7 +31,20 @@ php migrator.php  #migrate all indexes
 ```php
 php migrator.php  --es_host=127.0.0.1 --es_port=9200 --indexes=index1,index2
 ```
- 
+
+Parameters
+----------
+* `indexes` - list of ES index names, separated by comma. Default:  migrate all indexes available
+* `dryrun` - perform a dry run without migrating anything. It prints information about available ES indexes
+* `es_host` - ES host, default: 127.0.0.1
+* `es_port` - ES port, default: `92000`
+* `es_user` - ES username, no default 
+* `es_pass` - ES password, no default
+* `es_batch_size` - How many ES documents to retrieve per round (default 10000)  
+* `ms_host` - Manticore host, default: 127.0.0.1
+* `ms_port` - Manticore HTTP port, default: 9308
+* `ms_batch_size` - How many documents to group in a single INSERT batch in Manticore (default 10000)
+* `limit` - limit the number of documents from an index for migration (default 0 - migrate all )
 
 TODO
 ----
--- migrator.php
+++ migrator.php
@@ -5,37 +5,41 @@ require 'vendor/autoload.php';
 //$esi = $migrator->getESIndex(['index'=>'optimize_perf']);
 $cli = new Garden\Cli\Cli();
 $cli->opt('indexes', 'Indexes', false)
-    ->opt('dryrun','Show only schemas,no real import',false,'boolean')
-    ->opt('es_host', 'Elastic host', false)
-    ->opt('es_port', 'Elastic port', false)
-    ->opt('es_user', 'Elastic user', false)
-    ->opt('es_pass', 'Elastic pass', false)
-    ->opt('ms_host', 'Manticore host', false)
-    ->opt('ms_port', 'Manticore port', false);
+    ->opt('dryrun', 'Show only schemas,no real import', false, 'boolean')
+    ->opt('elasticsearch.host', 'Elastic host', false)
+    ->opt('elasticsearch.port', 'Elastic port', false)
+    ->opt('elasticsearch.user', 'Elastic user', false)
+    ->opt('elasticsearch.pass', 'Elastic pass', false)
+    ->opt('elasticsearch.batch_size', 'Elastic get batch size', false)
+    ->opt('limit', 'Limit number of documents to retrieve', false)
+    ->opt('manticoresearch.host', 'Manticore host', false)
+    ->opt('manticoresearch.port', 'Manticore port', false)
+    ->opt('manticoresearch.batch_size', 'Manticore insert batch size', false);
 $args = $cli->parse($argv, true);
-if(isset($args['indexes']) && $args['indexes'] !=="") {
+if (isset($args['indexes']) && $args['indexes'] !== "") {
     $indexes = explode(',', $args['indexes']);
-}else{
+} else {
     $indexes = [];
 }
 
 
 $config = [];
-if (isset($args['es_host'])) {
-    $config['elasticsearch'] = [
-        'host' => $args['es_host'],
-        'port' => $args['es_port'],
-        'user' => $args['es_user'],
-        'pass' => $args['es_pass']
-    ];
-}
-if (isset($args['ms_host'])) {
-    $config['manticoresearch'] = [
-        'host' => $args['ms_host'],
-        'port' => $args['ms_port']
-    ];
-}
-if(isset($args['dryrun'])) {
+
+$config['elasticsearch'] = [
+    'host' => $args->getOpt('elasticsearch.host', 'http://127.0.0.1'),
+    'port' => $args->getOpt('elasticsearch.port', 9200),
+    'user' => $args->getOpt('elasticsearch.user', ''),
+    'pass' => $args->getOpt('elasticsearch.pass', ''),
+    'batch_size' => $args->getOpt('elasticsearch.batch_size', 10000),
+];
+$config['limit'] =$args->getOpt('limit', 0);
+$config['manticoresearch'] = [
+    'host' => $args->getOpt('manticoresearch.user', '127.0.0.1'),
+    'port' => $args->getOpt('manticoresearch.port', 9308),
+    'batch_size' => $args->getOpt('manticoresearch.batch_size', 10000)
+];
+
+if (isset($args['dryrun'])) {
     $config['dryrun'] = $args['dryrun'];
 }
 $migrator = new Manticoresearch\ESMigrator($config);
